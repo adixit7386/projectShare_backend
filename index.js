a90569e2a376e0ed19eb5d565196bc9823ce3b0b
Abhay Dixit <adixit7386@gmail.com>
2023-03-08 12:33:34 +0530

    added cors origin to frontend

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://projectshare.netlify.app/",
   },
 });
 io.on("connection", (socket) => {
