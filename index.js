ac438d23739ccc0843bc73a96e4a3e4ec55f8f7e
Abhay Dixit <adixit7386@gmail.com>
2023-03-08 12:38:22 +0530

    corrected socket.io endpoint

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://projectshare.netlify.app/",
+    origin: "https://projectshare.netlify.app",
   },
 });
 io.on("connection", (socket) => {
